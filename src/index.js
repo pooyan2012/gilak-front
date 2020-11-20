554a619205e9b0ee71a0ab29b8bf0569fd6f8626
pooyan2012 <you@example.com>
2020-11-20 15:27:48 +0330

    3. Menu and active links

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//3. Menu and active links
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./Routes";
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
